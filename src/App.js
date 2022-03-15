238017542c3a8d02367bbc145d6817774ba8b14a
X-DSI <xmandsi@gmail.com>
2022-03-15 19:13:16 +0530

    Sucessfully Tested Dark/Light Theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import { ThemeProvider } from "styled-components"
 import styled from "styled-components"
 import PropTypes from 'prop-types'
 import { StyledButton } from "./styles/App.styled"
+import { GlobalStyles } from "./styles/globalstyles"
+import { lightTheme, darkTheme } from "./styles/Themes"
+import React, { useState, useEffect } from "react"
 
 const App = () => {
-  return (
-    <div>
-            <StyledButton>Click me</StyledButton>
-            <StyledButton secondary>Click for secondary Action</StyledButton>
-        </div>
-  )
+  const [theme, setTheme] = useState('light');
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
 }
+
+
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+      <GlobalStyles/>
+        <div className="App">
+          <button onClick={themeToggler}>Switch Theme</button>
+          {
+            }
+        </div>
+      </>
+    </ThemeProvider>
+    // <div>
+    //   <StyledButton>Click me</StyledButton>
+    //   <StyledButton secondary>Click for secondary Action</StyledButton>
+    // </div>
+  );
+};
 
 export default App
