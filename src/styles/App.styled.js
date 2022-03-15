092692fafe30a33ab9e3a406e67b6ee37b59b308
X-DSI <xmandsi@gmail.com>
2022-03-15 19:23:14 +0530

    Moved button to Horizontal Center

--- src/styles/App.styled.js
+++ src/styles/App.styled.js
@@ -1,12 +1,5 @@
 import styled from "styled-components"
 
 export const StyledButton = styled.button`
-  border-radius: 5px;
-  background-color: ${props => (props.secondary ? '#000000' : 'red')};
-  color: #fff;
-  padding: 10px 15px;
-  outline: none;
-  border: none;
-  cursor: pointer;
-  margin: 15px;
+  
 `
--- src/styles/globalstyles.js
+++ src/styles/globalstyles.js
@@ -8,5 +8,21 @@ export const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.text};
     font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
     transition: all 0.50s linear;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  Button{
+    background: #000;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: inherit;
   }
   `
